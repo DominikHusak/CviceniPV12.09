195853717b0b2f2276aac4a5260f3846e27680a6
DominikHusak <85923017+DominikHusak@users.noreply.github.com>
2022-09-12 09:08:11 +0200

    Add files via upload

---
+++ Cviceni.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CviceniPV12._09
+{
+    internal class Cviceni
+    {
+        static void Main(string[] args)
+        { 
+            Interval inter = new Interval(3, 9);
+            Interval inter2 = new Interval(11, 21);
+            Console.WriteLine(inter);
+            Console.WriteLine(inter.Numfind(5));
+        }
+    }
+}
---
+++ Interval.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CviceniPV12._09
+{
+    internal class Interval
+    {
+        public int leftBorder { get; set; }
+
+        public int rightBorder { get; set; }
+
+        public Interval(int leftBorder, int rightBorder)
+        {
+            this.leftBorder = leftBorder;
+            this.rightBorder = rightBorder;
+        }
+
+        public override string ToString()
+        {
+            return "Interval: [" + leftBorder + " " + rightBorder + "]";
+        }
+
+
+        public bool Numfind(int num)
+        {
+            return num >= leftBorder && num <= rightBorder;
+        }
+        //public Interval? PrunikIntervalu()
+        //{
+        //}
+
+    }
+}
